d52d0145a8fae7276fdfe10775874502eae07f4c
Ps19636 <Atulsolanki3238@gmail.com>
2025-02-18 23:40:42 +0530

    Update s.py

--- s.py
+++ s.py
@@ -307,8 +307,8 @@ def handle_bgmi(message):
             target = command[1]
             port = int(command[2])  # Convert port to integer
             time = int(command[3])  # Convert time to integer
-            if time > 360:
-                response = "Error: Time interval must be less than 360."
+            if time > 220:
+                response = "Error: Time interval must be less than 220."
             else:
                 record_command_logs(user_id, '/megoxer', target, port, time)
                 log_command(user_id, target, port, time)
